c0a65cda8aaf61c4e34c8aa476ff93debb34e84e
Your Name <ijazbi@gmail.com>
2019-06-04 18:44:37 -0700

    testing1

--- my.php
+++ my.php
@@ -2,27 +2,19 @@
 
  if (isset($_POST['submit'])) {
      $name= $_POST['name'];
-     $visitor_email= $_POST['email'];
+     $subject = $_POST['subject'];
+     $mailFrom= $_POST['mail'];
      $message = $_POST['message'];
-
-
-     $email_from = 'EasyTutorials@avinashkr.com';
-
-     $email_subject = "New Form Submission";
-
-     $email_body = "User Name: $name.\n".
-                      "User Email: $visitor_email.\n".
-                          "User Message: $message.\n"; 
      
 
-$emailto = "ijazbi@gmail.com";
+$mailto = "ijazbi@gmail.com";
 
-$headers = "From: ".$email_from;
+$headers = "From: ".$mail_from;
 
-$txt = "You have received mail from " .$name.".\n\n".$message;
+$txt = "You have received mail from ".$name.".\n\n".$message;
 
-mail($mailto,$txt,$headers);
+mail($mailto, $subject, $txt, $headers);
 
-header("Location: contact.html");
+header("Location: contact.php?mailsend");
 
 }
